fc43b2c4b68e5fda699e9253e8d23c254e44dcf2
arummuktip <44842042+arummuktip@users.noreply.github.com>
2019-04-20 12:27:02 +0700

    The WHERE statement

--- SQL_Reporting and Analysis/fruits table.sql
+++ SQL_Reporting and Analysis/fruits table.sql
@@ -1,1 +1,13 @@
-SELECT * FROM fruits.fruits;
+select * from fruits.fruitstable;
+
+select fruit_id, fruit_name from fruits.fruitstable;
+
+select * from fruitstable;
+
+select *  from fruits.fruitstable where unit_id = 1;
+select * from fruits.fruitstable;
+select inventory from fruits.fruitstable where unit_id != 6;
+select * from fruits.fruitstable;
+select fruit_name from fruits.fruitstable where unit_id between 3 and 6;
+select * from fruits.fruitstable;
+select * from fruits.fruitstable where fruit_name = 'strawberry';
