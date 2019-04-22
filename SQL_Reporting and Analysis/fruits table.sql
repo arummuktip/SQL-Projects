aca3f00a7547c3b373aca62f12a1a069cf322486
arummuktip <44842042+arummuktip@users.noreply.github.com>
2019-04-22 23:57:51 +0700

    innitial commit

--- SQL_Reporting and Analysis/fruits table.sql
+++ SQL_Reporting and Analysis/fruits table.sql
@@ -10,4 +10,4 @@ select inventory from fruits.fruitstable where unit_id != 6;
 select * from fruits.fruitstable;
 select fruit_name from fruits.fruitstable where unit_id between 3 and 6;
 select * from fruits.fruitstable;
-select * from fruits.fruitstable where fruit_name = 'strawberry';
+select * from fruits.fruitstable where fruit_name = 'strawberry';fruit_id
