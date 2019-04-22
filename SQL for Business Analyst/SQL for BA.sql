3eaacb4507158e2806c248b74721fe6fa7b8e2ee
arummuktip <44842042+arummuktip@users.noreply.github.com>
2019-04-22 14:10:01 +0700

    The WHERE statement

--- SQL for Business Analyst/SQL for BA.sql
+++ SQL for Business Analyst/SQL for BA.sql
@@ -1,1 +1,3 @@
-SELECT employee_id, first_name, last_name FROM employees.employeestable;
+SELECT * FROM employees.employeestable where salary >= 1600;
+select employee_id, first_name, last_name, salary
+ from employees.employeestable where first_name like 'j%';
