ae757032b557b44bf249e45f26fcff212c8f6b8d
arummuktip <44842042+arummuktip@users.noreply.github.com>
2019-04-22 21:27:24 +0700

    The HAVING Clause

--- SQL for Business Analyst/SQL for BA.sql
+++ SQL for Business Analyst/SQL for BA.sql
@@ -1,3 +1,6 @@
-select avg(salary), last_name from  employees.employeestable 
-where salary >= 1500 group by last_name;
-select sum(salary) from employees.employeestable;
+select avg(salary) as average_salary, first_name 
+from employees.employeestable
+where salary >= 1500
+group by first_name
+having count(first_name) != 'j%'
+order by average_salary desc;
