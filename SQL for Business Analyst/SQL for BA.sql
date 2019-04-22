d6f9443066e7c67e415df65df854549b40c7fed6
arummuktip <44842042+arummuktip@users.noreply.github.com>
2019-04-22 22:18:02 +0700

    INNERQuery

--- SQL for Business Analyst/SQL for BA.sql
+++ SQL for Business Analyst/SQL for BA.sql
@@ -1,6 +1,5 @@
-select avg(salary) as average_salary, first_name 
-from employees.employeestable
-where salary >= 1500
-group by first_name
-having count(first_name) != 'j%'
-order by average_salary desc;
+select avg(salary) as average_salary, last_name from employees.employeestable 
+where last_name 
+IN ( select last_name from employees.employeestable where last_name like '%s') 
+group by last_name;
+
